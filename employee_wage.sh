127e7f9ce4a0617846be3dcfe6064aa48e94fc8e
Manojgour05 <manojgour6811@gmail.com>
2020-10-02 00:35:43 +0530

    Refactor:check employee present or not

--- employee_wage.sh
+++ employee_wage.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+# check employee attendane
+        isPresent=1
+	randomCheck=$((1+Random%2))
+	if [ $isPresent -eq $randomCheck ]
+	then
+		echo "Employee is present"
+	else
+		echo "Employee is absent"
+	fi
